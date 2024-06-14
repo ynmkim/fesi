7f0ba671262b38bdf76eb568c9e25365ce419e59
김윤미 <younmeet@gmail.com>
2024-06-14 14:06:25 +0900

    feat: ScrollYProgress 인터랙티브 UI 구현

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,129 @@
+import { useSpring, useScroll, animated } from '@react-spring/web';
+import { useEffect, useState } from 'react';
+
 export default function Home() {
-  return <main></main>;
+  const { scrollYProgress } = useScroll();
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    handleScroll();
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const props = useSpring({
+    scaleX: scrollYProgress,
+    display: isScrolled ? 'block' : 'none',
+  });
+
+  return (
+    <main>
+      <animated.div
+        style={props}
+        className={`fixed top-0 left-0 right-0 h-[10px] bg-lime-500 origin-[0%]`}
+      ></animated.div>
+
+      <h1 className="text-[56px]">
+        <code>useScroll</code> demo
+      </h1>
+      <article>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ac
+          rhoncus quam.
+        </p>
+        <p>
+          Fringilla quam urna. Cras turpis elit, euismod eget ligula quis,
+          imperdiet sagittis justo. In viverra fermentum ex ac vestibulum.
+          Aliquam eleifend nunc a luctus porta. Mauris laoreet augue ut felis
+          blandit, at iaculis odio ultrices. Nulla facilisi. Vestibulum cursus
+          ipsum tellus, eu tincidunt neque tincidunt a.
+        </p>
+        <h2>Sub-header</h2>
+        <p>
+          In eget sodales arcu, consectetur efficitur metus. Duis efficitur
+          tincidunt odio, sit amet laoreet massa fringilla eu.
+        </p>
+        <p>
+          Pellentesque id lacus pulvinar elit pulvinar pretium ac non urna.
+          Mauris id mauris vel arcu commodo venenatis. Aliquam eu risus arcu.
+          Proin sit amet lacus mollis, semper massa ut, rutrum mi.
+        </p>
+        <p>Sed sem nisi, luctus consequat ligula in, congue sodales nisl.</p>
+        <p>
+          Vestibulum bibendum at erat sit amet pulvinar. Pellentesque pharetra
+          leo vitae tristique rutrum. Donec ut volutpat ante, ut suscipit leo.
+        </p>
+        <h2>Sub-header</h2>
+        <p>
+          Maecenas quis elementum nulla, in lacinia nisl. Ut rutrum fringilla
+          aliquet. Pellentesque auctor vehicula malesuada. Aliquam id feugiat
+          sem, sit amet tempor nulla. Quisque fermentum felis faucibus, vehicula
+          metus ac, interdum nibh. Curabitur vitae convallis ligula. Integer ac
+          enim vel felis pharetra laoreet. Interdum et malesuada fames ac ante
+          ipsum primis in faucibus. Pellentesque hendrerit ac augue quis
+          pretium.
+        </p>
+        <p>
+          Morbi ut scelerisque nibh. Integer auctor, massa non dictum tristique,
+          elit metus efficitur elit, ac pretium sapien nisl nec ante. In et ex
+          ultricies, mollis mi in, euismod dolor.
+        </p>
+        <p>Quisque convallis ligula non magna efficitur tincidunt.</p>
+        <p>
+          Pellentesque id lacus pulvinar elit pulvinar pretium ac non urna.
+          Mauris id mauris vel arcu commodo venenatis. Aliquam eu risus arcu.
+          Proin sit amet lacus mollis, semper massa ut, rutrum mi.
+        </p>
+        <p>Sed sem nisi, luctus consequat ligula in, congue sodales nisl.</p>
+        <p>
+          Vestibulum bibendum at erat sit amet pulvinar. Pellentesque pharetra
+          leo vitae tristique rutrum. Donec ut volutpat ante, ut suscipit leo.
+        </p>
+        <h2>Sub-header</h2>
+        <p>
+          Maecenas quis elementum nulla, in lacinia nisl. Ut rutrum fringilla
+          aliquet. Pellentesque auctor vehicula malesuada. Aliquam id feugiat
+          sem, sit amet tempor nulla. Quisque fermentum felis faucibus, vehicula
+          metus ac, interdum nibh. Curabitur vitae convallis ligula. Integer ac
+          enim vel felis pharetra laoreet. Interdum et malesuada fames ac ante
+          ipsum primis in faucibus. Pellentesque hendrerit ac augue quis
+          pretium.
+        </p>
+        <p>
+          Morbi ut scelerisque nibh. Integer auctor, massa non dictum tristique,
+          elit metus efficitur elit, ac pretium sapien nisl nec ante. In et ex
+          ultricies, mollis mi in, euismod dolor.
+        </p>
+        <p>Quisque convallis ligula non magna efficitur tincidunt.</p>
+        <h2>Sub-header</h2>
+        <p>
+          Maecenas quis elementum nulla, in lacinia nisl. Ut rutrum fringilla
+          aliquet. Pellentesque auctor vehicula malesuada. Aliquam id feugiat
+          sem, sit amet tempor nulla. Quisque fermentum felis faucibus, vehicula
+          metus ac, interdum nibh. Curabitur vitae convallis ligula. Integer ac
+          enim vel felis pharetra laoreet. Interdum et malesuada fames ac ante
+          ipsum primis in faucibus. Pellentesque hendrerit ac augue quis
+          pretium.
+        </p>
+        <p>
+          Morbi ut scelerisque nibh. Integer auctor, massa non dictum tristique,
+          elit metus efficitur elit, ac pretium sapien nisl nec ante. In et ex
+          ultricies, mollis mi in, euismod dolor.
+        </p>
+        <p>Quisque convallis ligula non magna efficitur tincidunt.</p>
+      </article>
+    </main>
+  );
 }
